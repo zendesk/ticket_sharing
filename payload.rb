a1188ab6919ab903ab171266bb092ac2dee322f1
Josh Lubaway <josh@zendesk.com>
2010-12-13 11:37:23 -0800

    TicketSharing takes a hash instead.
    Rails will give us the json parsing for free. When we need access to
    this payload object, we'll probably end up passing it the params
    hash.

--- payload.rb
+++ payload.rb
@@ -1,28 +1,20 @@
-require 'yajl'
-
 module TicketSharing
   class Payload
-
-    def self.parse_json(json)
-      parser = Yajl::Parser.new
-      result = parser.parse(json)
-      new(result)
-    end
 
     def initialize(hash)
       @hash = hash
     end
 
     def valid?
-      @hash['uuid']
+      @hash[:uuid]
     end
 
     def ticket_attributes
       {
-        'description' => @hash['description'],
-        'priority'    => @hash['priority'],
-        'status'      => @hash['status'],
-        'subject'     => @hash['subject']
+        :description => @hash['description'],
+        :priority    => @hash['priority'],
+        :status      => @hash['status'],
+        :subject     => @hash['subject']
       }
     end
 
