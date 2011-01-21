6be5459be1724edeff5e0ea02d8b4dbefec539c3
Josh Lubaway <josh@zendesk.com>
2011-01-20 16:53:21 -0800

    Adding a requester to TicketSharing::Ticket
    The requester can serialize and deserialize itself properly.

--- ticket.rb
+++ ticket.rb
@@ -1,17 +1,24 @@
 require 'ticket_sharing/base'
 require 'ticket_sharing/client'
+require 'ticket_sharing/actor'
 require 'ticket_sharing/agreement'
 
 module TicketSharing
   class Ticket < Base
 
-    fields :uuid, :subject, :description, :requested_at, :status
+    fields :uuid, :subject, :description, :requested_at, :status, :requester
 
     attr_accessor :agreement
 
     def self.parse(json)
       attributes = JsonSupport.decode(json)
       ticket = new(attributes)
+
+      if ticket.requester
+        ticket.requester = Actor.new(ticket.requester)
+      end
+
+      ticket
     end
 
     def send_to(url)
