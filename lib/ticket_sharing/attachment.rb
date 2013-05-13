6e6a6c9022c29919d3df9ada5c41086ce188b072
Marcos Oliveira <marcosvm@gmail.com>
2013-05-13 12:12:29 -0700

    add display_filename to the list of attachments fields shared

--- lib/ticket_sharing/attachment.rb
+++ lib/ticket_sharing/attachment.rb
@@ -3,7 +3,7 @@ require 'ticket_sharing/base'
 module TicketSharing
   class Attachment < Base
 
-    fields :url, :filename, :content_type
+    fields :url, :filename, :content_type, :display_filename
 
   end
 end
