a0648c27b134cec0f0683f0f300907f9753dd4ac
Ryan Davis <ryand-ruby@zenspider.com>
2023-06-21 12:56:16 -0700

    actor_test

--- test/models/actor_test.rb
+++ test/models/actor_test.rb
@@ -1,11 +1,13 @@
-require 'spec_helper'
+require 'test_helper'
 require 'ticket_sharing/actor'
 
 describe TicketSharing::Actor do
+  let(:described_class) { TicketSharing::Actor }
+
   it 'initializes from a hash' do
     hash = { 'uuid' => 'ba90d6', 'role' => 'staff' }
     actor = described_class.new(hash)
-    expect(actor.role).to eq('staff')
+    expect(actor.role).must_equal('staff')
   end
 
   it 'implements to_json' do
@@ -13,16 +15,16 @@ describe TicketSharing::Actor do
     actor = described_class.new(hash)
 
     actual = actor.to_json
-    expect(actual).to match(/ba90d6/)
-    expect(actual).to match(/Joe/)
+    expect(actual).must_match(/ba90d6/)
+    expect(actual).must_match(/Joe/)
   end
 
   it 'responds to actor' do
     actor = described_class.new
-    expect(actor).to_not be_agent
+    expect(actor).wont_be :agent?
 
     actor.role = 'agent'
-    expect(actor).to be_agent
+    expect(actor).must_be :agent?
   end
 
 end
