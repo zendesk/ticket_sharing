6d847e7088010c56d8bf0a7773a5ba0cf8f5a4de
Ryan Davis <ryand-ruby@zenspider.com>
2023-06-21 12:57:30 -0700

    base_test

--- test/models/base_test.rb
+++ test/models/base_test.rb
@@ -1,17 +1,17 @@
-require 'spec_helper'
+require 'test_helper'
 require 'ticket_sharing/base'
 
 describe TicketSharing::Base do
 
   it 'inherits fields list from ancestors' do
-    klass1 = Class.new(described_class) do
+    klass1 = Class.new(TicketSharing::Base) do
       fields :foo, :bar
     end
 
     klass2 = Class.new(klass1)
 
-    expect(klass1.new.field_list).to eq([:foo, :bar])
-    expect(klass2.new.field_list).to eq([:foo, :bar])
+    expect(klass1.new.field_list).must_equal([:foo, :bar])
+    expect(klass2.new.field_list).must_equal([:foo, :bar])
   end
 
 end
